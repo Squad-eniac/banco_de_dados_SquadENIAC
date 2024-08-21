888bd6c8b9e4d9a5aba4a78abd61cb923a6ed645
Ignacio-fabianamaria <faby.tbe@gmail.com>
2024-08-21 11:00:14 -0300

    feat(db): add records to Livro_Autor, Livro_Genero, Emprestimo, and Autor tables

---
+++ registros.py
@@ -0,0 +1,86 @@
+exemplares = [
+    (1, True, 0),
+    (1, True, 0),
+    (2, True, 0),
+    (2, True, 0),
+    (2, True, 0),
+    (3, True, 0),
+    (4, True, 0),
+    (4, True, 0),
+    (5, True, 0),
+    (6, True, 0),
+    (7, True, 0),
+    (8, True, 0),
+    (9, True, 0),
+    (10, True, 0),
+    (11, True, 0),
+    (11, True, 0),
+    (11, True, 0),
+]
+
+pessoas_autores = [
+    ("J.K. Rowling",),
+    ("George R.R. Martin",),
+    ("J.R.R. Tolkien",),
+    ("Agatha Christie",),
+    ("Dan Brown",),
+    ("Stephen King",),
+    ("Paulo Coelho",),
+    ("Isaac Asimov",),
+    ("Arthur C. Clarke",),
+    ("H.G. Wells",),
+]
+
+autores = [
+    (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19,), (20,), (21,)
+    ]
+
+emprestimos = [
+    (1, 1, '2024-07-01', '2024-07-15', 'Devolvido'),
+    (2, 2, '2024-08-02', '2024-08-25', 'Em aberto'),
+    (3, 3, '2024-07-05', '2024-07-20', 'Devolvido'),
+    (4, 4, '2024-08-04', '2024-08-30', 'Em aberto'),
+    (5, 5, '2024-07-10', '2024-07-25', 'Devolvido'),
+    (6, 6, '2024-08-06', '2024-08-28', 'Em aberto'),
+    (7, 7, '2024-08-07', '2024-08-31', 'Em aberto'),
+    (8, 8, '2024-08-08', '2024-09-05', 'Em aberto'),
+    (9, 9, '2024-08-09', '2024-09-07', 'Em aberto'),
+    (10, 10, '2024-08-10', '2024-08-24', 'Devolvido'),
+    (11, 11, '2024-08-11', '2024-08-26', 'Em aberto'),
+    (12, 1, '2024-08-12', '2024-09-01', 'Em aberto'),
+    (13, 2, '2024-08-13', '2024-09-02', 'Em aberto'),
+    (14, 3, '2024-08-14', '2024-09-03', 'Em aberto'),
+    (15, 4, '2024-08-15', '2024-09-04', 'Em aberto'),
+    (16, 5, '2024-08-16', '2024-09-06', 'Em aberto'),
+    (17, 6, '2024-08-17', '2024-09-07', 'Em aberto'),
+]
+
+livro_autor = [
+    (1, 12),  # Livro com id = 1 associado ao Autor com id = 12
+    (1, 13),  # Livro com id = 1 associado ao Autor com id = 13
+    (2, 14),  # Livro com id = 2 associado ao Autor com id = 14
+    (3, 15),  # Livro com id = 3 associado ao Autor com id = 15
+    (4, 16),  # Livro com id = 4 associado ao Autor com id = 16
+    (5, 17),  # Livro com id = 5 associado ao Autor com id = 17
+    (6, 18),  # Livro com id = 6 associado ao Autor com id = 18
+    (7, 19),  # Livro com id = 7 associado ao Autor com id = 19
+    (8, 20),  # Livro com id = 8 associado ao Autor com id = 20
+    (9, 21),  # Livro com id = 9 associado ao Autor com id = 21
+    (10, 12),  # Livro com id = 10 associado ao Autor com id = 12
+    (11, 13)  # Livro com id = 11 associado ao Autor com id = 13
+]
+
+livro_genero = [
+    (1, 1),  # Livro com id = 1 associado ao Gênero com id = 1
+    (1, 2),  # Livro com id = 1 associado ao Gênero com id = 2
+    (2, 3),  # Livro com id = 2 associado ao Gênero com id = 3
+    (3, 4),  # Livro com id = 3 associado ao Gênero com id = 4
+    (4, 5),  # Livro com id = 4 associado ao Gênero com id = 5
+    (5, 6),  # Livro com id = 5 associado ao Gênero com id = 6
+    (6, 7),  # Livro com id = 6 associado ao Gênero com id = 7
+    (7, 8),  # Livro com id = 7 associado ao Gênero com id = 8
+    (8, 9),  # Livro com id = 8 associado ao Gênero com id = 9
+    (9, 10),  # Livro com id = 9 associado ao Gênero com id = 10
+    (10, 1),  # Livro com id = 10 associado ao Gênero com id = 1
+    (11, 2)   # Livro com id = 11 associado ao Gênero com id = 2
+]
